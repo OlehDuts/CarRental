06371a657189e0d4abe99d8b8ba6cdf8834a2e69
OlehDuts <oleh.duts@gmail.com>
2016-04-04 00:00:30 +0300

    add collection

--- client/main.js
+++ client/main.js
@@ -1,7 +1,3 @@
-import { Accounts } from 'meteor/accounts-base';
- 
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
-import '../imports/startup/accounts-config.js';
-import '../imports/ui/body.js';
