be12d0c6a68df52231c249a2e5744620805d2410
OlehDuts <oleh.duts@gmail.com>
2016-04-26 00:15:21 +0300

    new filter

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,2 +1,22 @@
 Meteor.subscribe('Cars');
 Meteor.subscribe('Cities');
+
+Template.home.events({
+    'submit': function(e,tmp){
+        e.preventDefault();
+        Session.set('cityFilter', $('#city').val());
+    }
+});
+
+Template.home.helpers({
+    cars: function(){
+		if (Session.get('cityFilter') === '' || Session.get('cityFilter') === undefined) {
+  			return Cars.find();
+		} 
+		else {
+    		return Cars.find({
+      		city: Session.get('cityFilter'),
+    		});
+		}
+	}
+});
